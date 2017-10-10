7a7ad42b10fff2a36c4f98a651386db5c04eaed1
oppara <oppara@oppara.tv>
2017-10-11 00:28:21 +0900

    Handle json

--- php/send.php
+++ php/send.php
@@ -1,18 +1,30 @@
 <?php
 require_once __DIR__ . '/bootstrap.php';
 
+function makeBody($idx)
+{
+    $tmp =  [
+        'id' => Ramsey\Uuid\Uuid::uuid4()->toString(),
+        'body' =>  "body $idx"
+    ];
+
+    return json_encode($tmp);
+}
+
+
 try {
     $client = createClient();
 
     for ($i = 0; $i < 5; $i++) {
+        $body = makeBody($i);
         $client->sendMessage([
             'QueueUrl' => QUEUE_URL,
-            'MessageBody' => 'body ' . hash('sha256', time() . $i),
+            'MessageBody' => $body,
             'MessageGroupId' => 'php',
-            // MessageBodyが重複せず、 「コンテンツに基づく重複排除」にチェックを入れた場合
-            // MessageBodyの内容をsha256して自動で「MessageDeduplicationId」に設定してくれるので
-            // MessageDeduplicationIdの指定は不要
-            // MessageBodyが重複する場合は、MessageDeduplicationIdは必要
+            // // MessageBodyが重複せず、 「コンテンツに基づく重複排除」にチェックを入れた場合
+            // // MessageBodyの内容をsha256して自動で「MessageDeduplicationId」に設定してくれるので
+            // // MessageDeduplicationIdの指定は不要
+            // // MessageBodyが重複する場合は、MessageDeduplicationIdは必要
             // 'MessageBody' => 'body ' . $i,
             // 'MessageDeduplicationId' => hash('sha256', time() . $i),
         ]);
